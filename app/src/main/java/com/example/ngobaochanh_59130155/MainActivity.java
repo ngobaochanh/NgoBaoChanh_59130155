d6f1abfbc7c58e95c6b22fa6e1fd0bd6592e1258
DELL <tranducluong2811@gmail.com>
2020-04-11 20:10:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ngobaochanh_59130155/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ngobaochanh_59130155;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
